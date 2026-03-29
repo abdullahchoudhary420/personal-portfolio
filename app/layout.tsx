089fd7b740bea0e4b896b1314144bda59fc47560
Abdullah Choudhary <abdullah@192.168.1.9>
2026-03-29 18:48:05 +0500

    title fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import './globals.css'; // Global styles
 
 export const metadata: Metadata = {
-  title: 'Abdullah Noouman',
-  description: 'Abdullah Noouman',
+  title: 'Abdullah Nouman',
+  description: 'Abdullah Nouman',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
